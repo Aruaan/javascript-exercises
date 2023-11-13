3a7995a67756203d44a19a3605d5b8b430980546
Aruaan <74672886+Aruaan@users.noreply.github.com>
2023-11-13 18:23:22 +0100

    Done.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (stringToBeReversed) {
+  let isAPalindrome = false
+  let originalString = ''
+  let reversedString = ''
+  for(i = 0; i < stringToBeReversed.length ; i++){
+    if(stringToBeReversed[i].match(/[a-zA-Z0-9]/)){
+      originalString += stringToBeReversed[i]
+    }
+  }
+  for(i = originalString.length - 1; i >= 0; i--){
+    reversedString +=originalString[i]
+  }
+  if(originalString.toLowerCase() == reversedString.toLowerCase()){
+    isAPalindrome = true
+  }
+  return isAPalindrome
 };
 
 // Do not edit below this line
