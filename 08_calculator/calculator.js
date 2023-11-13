47cf230c84dbf17098ce7830bff5a0c686fa05a9
Aruaan <74672886+Aruaan@users.noreply.github.com>
2023-11-13 18:07:03 +0100

    Finished.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum = 0
+	for(let i = 0; i < a.length; i++){
+    sum += a[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(a) { 
+  let product = a[0]
+  for(i = 1; i < a.length; i ++){
+    product *= a[i]
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1
+  for(i = 1; i <= a; i++){
+    total *= i
+  }
+  return total
 };
 
 // Do not edit below this line
