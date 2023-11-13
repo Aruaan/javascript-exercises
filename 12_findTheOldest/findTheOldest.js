a35cdd2ad4ff4fa1b882b00067c4e456d151754c
Aruaan <74672886+Aruaan@users.noreply.github.com>
2023-11-13 19:11:47 +0100

    Done.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let maxAge = 0;
+  let currentOldest = null;
+  const currentYear = new Date().getFullYear();
 
+  for(let i = 0; i < array.length; i++) {
+    let deathYear = array[i].yearOfDeath || currentYear; // Use currentYear if yearOfDeath is not provided
+    let age = deathYear - array[i].yearOfBirth;
+
+    if(age > maxAge) {
+      maxAge = age;
+      currentOldest = array[i];
+    }
+  }
+
+  return currentOldest;
 };
 
-// Do not edit below this line
+
+
+// Do not edit beloaw this line
 module.exports = findTheOldest;
